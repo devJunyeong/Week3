302366be4bff7e122d809e8bb0b2e823af820b60
Junyeon Hwang <202430334@email.daelim.ac.kr>
2024-09-25 14:46:05 +0900

    간단한 계산기를 만들어 입력한 두 수의 합 차, 곱, 나눗셈을 출력하는 프로그램

--- src/InputPractice.java
+++ src/InputPractice.java
@@ -5,16 +5,14 @@ public class InputPractice {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자: ");
-        double number = sc.nextDouble();
-
+        int first = sc.nextInt();
         System.out.print("두 번째 숫자: ");
-        int age = sc.nextInt();
-
+        int second = sc.nextInt();
         System.out.print("연산자(+, -, *, /): ");
-        String operator = sc.next();
+        String a = sc.next();
 
-        System.out.println("결과: 첫 번째 숫자 = " + number + ", 두 번째 숫자 = " + age + ", 연산자 = " + operator);
+        System.out.println("결과 : "+first +" "+ a +" "+ second);
+
         sc.close();
-
     }
 }
