b246fd2efc0571f3966a1d3ac3aff4dfd6bc8258
Junyeon Hwang <202430334@email.daelim.ac.kr>
2024-09-25 14:41:35 +0900

    Initial commit

---
+++ src/InputPractice.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class InputPractice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자: ");
+        double number = sc.nextDouble();
+
+        System.out.print("두 번째 숫자: ");
+        int age = sc.nextInt();
+
+        System.out.print("연산자(+, -, *, /): ");
+        String operator = sc.next();
+
+        System.out.println("결과: 첫 번째 숫자 = " + number + ", 두 번째 숫자 = " + age + ", 연산자 = " + operator);
+        sc.close();
+
+    }
+}
